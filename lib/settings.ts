cf438e152a8b9025673d31560d115239dac964bb
MoChain Official <112720649+mochainapp@users.noreply.github.com>
2022-10-18 21:24:04 +0200

    Update settings.ts

--- lib/settings.ts
+++ lib/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
   localStorageKey: "signed_token",
   SIGN_MESSAGE:
-    "I accept the CryptoEscrow Terms of Service: https://moescrow.app/terms-and-conditions",
+    "I accept the MOTrust Terms of Service: https://moescrow.app/terms-and-conditions",
   ipfsGateways: [
     "https://cloudflare-ipfs.com/ipfs/%s/%s",
     "https://%s.ipfs.dweb.link/%s",
